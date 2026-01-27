ff5b5390decd1d7b6c63b12d6d74bce24b3688fe
hutaspamz-hue <hutaspamz@gmail.com>
2026-01-27 16:02:39 +0400

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from gatet import Tele
 from datetime import datetime
 
 # Bot config
-BOT_TOKEN = "8426512661:AAGWiADKvrJHDp919MndSpBS6PDTAY5TZ6k"
+BOT_TOKEN = "8426512661:AAHwyHErP1BVX_Ph7A-02vMrzVZH4KLidsY"
 ALLOWED_CHAT_ID = "8202990461"
 
 bot = telebot.TeleBot(BOT_TOKEN, parse_mode="HTML")
